9658acb4ee7f6b06b9803675318d925294084c02
Nurdaulet <44773658+NurdauletBolatov@users.noreply.github.com>
2019-02-13 16:52:11 +0600

    Add files via upload

---
+++ WEEK1/Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    { 
+        public static bool inPrime(int b)     /*метод для того чтобы узнать простое ли число*/
+        {
+            if (b == 1)
+                return false;
+            for (int i = 2; i < b; i++)
+            {
+                if (b % i == 0)
+                    return false;
+
+            }
+            return true;                         /*возвращает true or  false*/
+
+                    
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());    /* поменяем символ на число с помощью convert*/
+            int[] a = new int[n];
+            int s = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int b = Convert.ToInt32(Console.ReadLine());
+                a[i] = b;
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (inPrime(a[i]))
+                    s++;
+            } Console.WriteLine(s);                  /*выводим сумму простых чисел*/
+            for (int i = 0; i < n; i++)
+            {
+                if (inPrime(a[i]))
+                    Console.Write(a[i]+" " );         /*выводится простые числа*/
+            }
+            }
+    }
+}
---
+++ WEEK1/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string ss = Console.ReadLine();
+            string[] a = ss.Split(' ');
+            int aa = int.Parse(s);
+            int[] c = new int[aa];
+            /* #1 Сперва я вожу символ потом превращаю его на цифру
+           используя int.Parse(), и вожу элементы массива через пробел 
+           для этого я использовал .Split()*/
+            for (int j = 0; j < aa; j++)
+            {
+
+                int b = int.Parse(a[j]);
+                c[j] = b;
+            }
+            /* #2 Это элементы массива.Каждый раз я преобразовал string на int  */
+            for (int j = 0; j < aa; j++)
+            {
+                Console.Write(c[j] + " " + c[j] + " "); /*#3 А тут я уже вывожу элементы массива на экран */
+            }
+            Console.ReadKey();
+        }
+    }
+}

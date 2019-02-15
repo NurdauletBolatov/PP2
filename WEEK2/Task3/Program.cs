4b9422ea8bf5f5d31082fb709ad7e99b0793f41c
Nurdaulet <44773658+NurdauletBolatov@users.noreply.github.com>
2019-02-16 02:54:43 +0600

    Update Program.cs

--- WEEK2/Task3/Program.cs
+++ WEEK2/Task3/Program.cs
@@ -12,7 +12,7 @@ namespace Task3
         static void Main(string[] args)
         {
             DirectoryInfo dir = new DirectoryInfo(@"C:\Users\User\Desktop"); //инфо папки
-            PrintInfo(dir, 0); //функция которая выводит все папки и файлы который содержит уик2 в лестничном порядке
+            PrintInfo(dir, 0); //функция которая выводит все папки и файлы который содержит Users в лестничном порядке
         }
 
         static void PrintInfo(FileSystemInfo fsi, int k)
@@ -25,7 +25,7 @@ namespace Task3
                 FileSystemInfo[] arr = ((DirectoryInfo)fsi).GetFileSystemInfos();
                 for (int i = 0; i < arr.Length; ++i)
                 {
-                    PrintInfo(arr[i], k + 3);  //это метод вызывает сам себя с другими параметрами
+                    PrintInfo(arr[i], k + 3);  //этоn метод вызывает сам себя с другими параметрами
                 }
                 Console.ReadKey();
             }

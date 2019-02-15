6ce3dc39039a50b46a713784cbe83291e37b7971
Nurdaulet <44773658+NurdauletBolatov@users.noreply.github.com>
2019-02-16 01:46:40 +0600

    Update Program.cs

--- WEEK1/Task3/Program.cs
+++ WEEK1/Task3/Program.cs
@@ -7,9 +7,8 @@ using System.Threading.Tasks;
 namespace Task3
 {
     class Program
-    {
-        public void RepeatedArr(List<int> a, int b)   //метод, добавляет число который к нему попал в новый "list" 2 раза 
-                                                       //класс List служит для работы со списками
+     
+    {public void RepeatedArr(List<int>a,int b)
         {
             a.Add(b);
             a.Add(b);
@@ -17,22 +16,21 @@ namespace Task3
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            
-            int[] arr1 = new int[n];                      //объявление первого массива
-            List<int> arr2 = new List<int>();             //объявление пустого листа(типа контейнер в с++)
-
+            int[] arr1 = new int[n];
+            List<int> arr2 = new List<int>();
+            string d = Console.ReadLine();
+            string [] dd = d.Split(' ');
             for (int i = 0; i < n; i++)
-            { 
-                
-                arr1[i]= int.Parse(Console.ReadLine());
-
-                Program p = new Program();
-                p.RepeatedArr(arr2, arr1[i]);              //используем метод(функцию)
-            }
-            for (int i = 0; i < 2 * n; i++)
             {
-                Console.Write(arr2[i] + " ");               //вывод
+                arr1[i] = int.Parse(dd[i]);
+                Program p = new Program();
+                p.RepeatedArr(arr2, arr1[i]);
             }
-        }
+              for (int i = 0; i < 2 * n; i++)
+            {
+                Console.Write(arr2[i]+" ");
+            }
+            Console.ReadKey();     
+         }
     }
 }

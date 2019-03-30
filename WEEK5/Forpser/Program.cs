4a95074d60f7807338ab79e93a554d59b624b573
Nurdaulet <44773658+NurdauletBolatov@users.noreply.github.com>
2019-03-30 08:35:50 +0600

    Add files via upload

---
+++ WEEK5/Forpser/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Forpser
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        //[XmlIgnore]
+        public string name;
+        public int age;
+        public void PrintInfo()
+        {
+            Console.WriteLine(name+" "+age);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
+        private static void F1()
+        {
+            Student s = new Student();
+            s.name = "nurdaulet";
+            s.age = 17;
+
+
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("1students.xml", FileMode.Create, FileAccess.Write);
+            xs.Serialize(fs, s);
+
+            fs.Close();
+        }
+        private static void F2()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("1students.xml", FileMode.Open, FileAccess.Read);
+            Student student = xs.Deserialize(fs) as Student;
+
+            student.PrintInfo();
+            fs.Close();
+        }
+
+    }
+}
---
+++ WEEK5/forpractice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Example2
+{
+    public class Student
+    {
+        public Student()
+        {
+
+        }
+        public string name;
+        public int age;
+        public  void PrintInfo()
+        {
+            Console.WriteLine(name+" "+age);
+        }
+            
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        
+         }
+        private static void F2()
+        {
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("student.xml", FileMode.Open, FileAccess.Read);
+            Student student=xs.Deserialize(fs) as Student;
+            student.PrintInfo();
+            fs.Close();
+        }
+        private static void F1()
+        {
+            Student s = new Student();
+            s.name = "Nurdaulet";
+            s.age = 17;
+
+            XmlSerializer xs = new XmlSerializer(typeof(Student));
+            FileStream fs = new FileStream("student.xml", FileMode.Create, FileAccess.Write);
+            xs.Serialize(fs, s);
+            fs.Close();
+        }
+    }
+}
